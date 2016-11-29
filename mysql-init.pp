75390cfe9fa109b0dda42644227720a92b80b469
leq8jx <sylars05@gmail.com>
2016-11-29 12:16:46 +0100

    Update mysql-init.pp

--- mysql-init.pp
+++ mysql-init.pp
@@ -1,6 +1,13 @@
 class { '::mysql::server':
   root_password           => 'rootpassword',
   remove_default_accounts => true,
+  override_options        => $override_options,
+}
+
+$override_options = {
+  'mysqld' => {
+    'bind-address' => '0.0.0.0',
+  }
 }
 
 mysql::db { hiera("dbtable"):
