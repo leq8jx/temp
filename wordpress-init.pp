817b185b7860cc17c1aa1fc22c9672bcd3453497
leq8jx <sylars05@gmail.com>
2016-12-02 15:13:44 +0100

    Update wordpress-init.pp

--- wordpress-init.pp
+++ wordpress-init.pp
@@ -1,6 +1,5 @@
 wordpress::instance { '/opt/wordpress1':
   db_user     => hiera("dbuser"),
-  db_name     => hiera("dbname"),
   db_password => hiera("dbpass"),
   db_host     => hiera("mysqlhost"),
 }
